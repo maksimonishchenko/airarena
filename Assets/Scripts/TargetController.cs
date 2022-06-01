cd06088452c360163d651cacadd07462fea4ef0c
DESKTOP-BRB141Q\PC <longeithershort@gmail.com>
2022-06-01 22:34:20 +0300

    roboprimarun

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Numerics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using Unity.MLAgents;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine.Events;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 namespace Unity.MLAgentsExamples
 {
@@ -12,9 +17,17 @@ namespace Unity.MLAgentsExamples
     /// </summary>
     public class TargetController : MonoBehaviour
     {
+         
+         [SerializeField] float радиусввиртметрахлинейных;
+         
         [SerializeField] LayerMask layerMask;
         [SerializeField] float rayDown = 10;
+        [SerializeField] Transform agentSpine;
 
+        [SerializeField] private float closearearadius = 1.44f;
+        public float d;
+        
+        
         public Vector2 spawnX = new Vector2(-10, 10); //The region in which a target can be spawned.
         public Vector2 spawnY = new Vector2(0.5f, 1.5f);
         public Vector2 spawnZ = new Vector2(-10, 10);
@@ -23,12 +36,94 @@ namespace Unity.MLAgentsExamples
 
         const string k_Agent = "agent";
 
+        private float[] буферугловградусов = new float[360];
+        
+        private int индексаторреальноговремени = 1;
+            
+        private void Awake()
+        {
+            var расстояние = 0f;
+            var индекс = 0;
+            
+            for (int i = 0; i < 360; i++)
+            {
+                буферугловградусов[i] = i;
+                расстояние = Vector3.Distance(расстояниядосекциисекунднойплощадкинаколизейномпериметре(i),agentSpine.transform.position);
+                if (расстояние > Vector3.Distance(расстояниядосекциисекунднойплощадкинаколизейномпериметре(i),
+                    agentSpine.transform.position))
+                {
+                    расстояние = Vector3.Distance(расстояниядосекциисекунднойплощадкинаколизейномпериметре(i),agentSpine.transform.position);
+                    индекс = i;
+                }
+                //доделать
+                
+            }
+
+            индексаторреальноговремени = 0;
+            transform.position = new Vector3(Mathf.Cos(буферугловградусов[индекс] * Mathf.Deg2Rad),0.5f,Mathf.Sin(буферугловградусов[индекс] * Mathf.Deg2Rad)) * радиусввиртметрахлинейных;
+        }
+
+        private Vector3 расстояниядосекциисекунднойплощадкинаколизейномпериметре(int уголвградусах)
+        {
+            return new Vector3(Mathf.Cos(буферугловградусов[уголвградусах] * Mathf.Deg2Rad),0.5f,Mathf.Sin(буферугловградусов[уголвградусах] * Mathf.Deg2Rad)) * радиусввиртметрахлинейных;
+        }
+
         void FixedUpdate()
         {
-            if (transform.localPosition.y < -5)
+            d = Vector3.Distance(transform.position, agentSpine.transform.position);
+            
+            //var lefty = Vector3.Dot(transform.position - agentSpine.transform.position, agentSpine.transform.forward);
+
+            var close = d < closearearadius;
+            //var near = Mathf.Abs(d) > 0.08f;
+            
+            if (close)
+            {
+                --индексаторреальноговремени;
+                    //индексаторреальноговремени = Mathf.Abs(индексаторреальноговремени);
+                индексаторреальноговремени %= 360;
+                var индексмассива = Mathf.Abs(индексаторреальноговремени);
+                
+                var абсцисс = Mathf.Cos(буферугловградусов[индексмассива] * Mathf.Deg2Rad);
+                var оординат = Mathf.Sin(буферугловградусов[индексмассива] * Mathf.Deg2Rad);
+                
+                transform.position = new Vector3(абсцисс,0.5f,оординат) * радиусввиртметрахлинейных;
+            }
+            //else --anglesdegsArrow;
+            
+            //anglesdegsArrow %= 360;
+            //anglesdegsArrow = Mathf.Abs(anglesdegsArrow);
+            //+=Time.deltaTime
+            
+            
+            //var far = Mathf.Abs(d) < 1f;
+            
+            //var yay = transform.TransformPoint(Vector3.Cross(agentSpine.transform.forward,Vector3.up) * 1f);
+            //var straight = agentSpine.transform.position + agentSpine.transform.forward * 1f;
+            //var backontrack  = new Vector3(Mathf.Cos(angle+=Time.deltaTime),0.5f,Mathf.Sin(angle+=Time.deltaTime)) * radius;
+            //
+            //if(close)
+            //{
+            //    transform.position = Vector3.Lerp(backontrack,transform.position,0.5f);
+            //    //transform.position = backontrack;
+            //}
+            //else if(near)
+            //{
+            //    transform.position = Vector3.Lerp(straight,transform.position,0.5f);
+            //} 
+            //
+            //if (close)
+            //{
+            //    transform.position = Vector3.Lerp(yay,transform.position,0.5f);
+            //}
+            
+            //if (Mathf.Abs(d) <= 0.08f)
+                
+            
+            if (transform.position.y < -5)
             {
                 Debug.Log($"{transform.name} Fell Off Platform");
-                MoveTargetToRandomPosition();
+                //MoveTargetToRandomPosition();
             }
         }
 
@@ -37,6 +132,8 @@ namespace Unity.MLAgentsExamples
         /// </summary>
         public void MoveTargetToRandomPosition()
         {
+            return;
+            
             Vector3 newTargetPos;
             Collider[] hitColliders;
 
